fd22b2302602293ffc7498ff15c17f58124f8c44
Sierra Obryan <sierra@atomicrobot.com>
2020-11-06 20:30:48 -0500

    add drawer

---
+++ app/src/main/java/com/sierraobryan/navigation_simple_app/ui/menus/BottomNavigationFragment.kt
@@ -0,0 +1,33 @@
+package com.sierraobryan.navigation_simple_app.ui.menus
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sierraobryan.navigation_simple_app.R
+
+class BottomNavigationFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottom_navigation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navView: BottomNavigationView = view.findViewById(R.id.bottomNavigation)
+        val navController = Navigation.findNavController(requireActivity(), R.id.bottom_nav_host_fragment)
+        navView.setupWithNavController(navController)
+        navView.setOnNavigationItemReselectedListener {
+            // Do nothing -- This prevents consecutive navigation to the same fragment
+        }
+    }
+}
--- app/src/main/java/com/sierraobryan/navigation_simple_app/ui/menus/ThirdActivity.kt
+++ app/src/main/java/com/sierraobryan/navigation_simple_app/ui/menus/ThirdActivity.kt
@@ -1,13 +1,23 @@
 package com.sierraobryan.navigation_simple_app.ui.menus
 
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.sierraobryan.navigation_simple_app.R
 
 class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +26,27 @@ class ThirdActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
                 .findFragmentById(R.id.third_nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val navView: BottomNavigationView = findViewById(R.id.bottomNavigation)
-        navView.setupWithNavController(navController)
-        navView.setOnNavigationItemReselectedListener {
-            // Do nothing -- This prevents consecutive navigation to the same fragment
-        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
 
+        supportActionBar?.title = "Menu Examples"
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.bottom_nav_fragment, R.id.menu_navigation_fragment), drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.third_nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
